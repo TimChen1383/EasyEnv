e954a264a32c39052c40480aef0d945e75ef0590
TimChen <37008451+TimChen1383@users.noreply.github.com>
2025-12-25 23:59:40 +0800

    add ml-sharp

---
+++ EasyEnv_1.0.0/ml-sharp/src/sharp/cli/__main__.py
@@ -0,0 +1,10 @@
+"""Entry point for running sharp.cli as a module.
+
+For licensing see accompanying LICENSE file.
+Copyright (C) 2025 Apple Inc. All Rights Reserved.
+"""
+
+from . import main_cli
+
+if __name__ == "__main__":
+    main_cli()
